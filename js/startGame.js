3f34157bedba44418c77d22115574969947a03b0
nilsmanc <etereaiberia@gmail.com>
2023-08-31 06:35:08 +0300

    game logic

--- js/startGame.js
+++ js/startGame.js
@@ -26,5 +26,49 @@ export const startGame = (difficult) => {
 
   const cards = document.querySelectorAll('.game-card')
 
-  cards.forEach((card, index) => card.addEventListener('click', () => {}))
+  cards.forEach((card, index) =>
+    card.addEventListener('click', () => {
+      if (clickable == true && !card.classList.contains('successfully')) {
+        card.classList.add('flip')
+
+        if (firstCard == null) {
+          firstCard = index
+        } else {
+          if (index != firstCard) {
+            secondCard = index
+            clickable = false
+          }
+        }
+
+        if (
+          firstCard != null &&
+          secondCard != null &&
+          firstCard != secondCard
+        ) {
+          if (
+            cards[firstCard].firstElementChild.className ===
+            cards[secondCard].firstElementChild.className
+          ) {
+            setTimeout(() => {
+              cards[firstCard].classList.add('successfully')
+              cards[secondCard].classList.add('successfully')
+
+              firstCard = null
+              secondCard = null
+              clickable = true
+            }, 500)
+          } else {
+            setTimeout(() => {
+              cards[firstCard].classList.remove('flip')
+              cards[secondCard].classList.remove('flip')
+
+              firstCard = null
+              secondCard = null
+              clickable = true
+            }, 500)
+          }
+        }
+      }
+    })
+  )
 }
